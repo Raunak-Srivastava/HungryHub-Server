0a5e6c0f640a59749124b207b3170563edae9114
Raunak-Srivastava <2021ugee081@nitjsr.ac.in>
2024-04-06 02:24:35 +0530

    updated contact router

--- routes/contact.js
+++ routes/contact.js
@@ -1,10 +1,8 @@
 import express from 'express';
-import { isAuthenticated } from '../middlewares/auth.js';
 import { contact } from '../controllers/contact.js';
 
 const router = express.Router();
 
-// router.post("/contact" , isAuthenticated , contact);
 router.post("/contact" , contact);
 
 export default router;
